7571093c4a490e8134f20c11274ec62e2a5444a8
Florian Stöhr <stewie002@gmail.com>
2021-12-01 20:21:09 +0100

    Initial commit

---
+++ src/Day01/Program.fs
@@ -0,0 +1,10 @@
+﻿open System
+
+[<EntryPoint>]
+let main args =
+    printfn "Nice command-line arguments! Here's what System.Text.Json has to say about them:"
+
+
+    args |> Array.iter (fun item -> printfn "%s" item)
+
+    0 // return an integer exit code
