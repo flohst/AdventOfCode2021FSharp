5287985741448faeacbeb573956637dd0a16cf3c
Florian Stöhr <stewie002@gmail.com>
2021-12-01 21:12:17 +0100

    Solve day one part one

--- src/Day01/Program.fs
+++ src/Day01/Program.fs
@@ -1,10 +1,41 @@
-﻿open System
+﻿open System.IO
+
+let readLines filename = File.ReadAllLines filename
+
+let isPreviousValueSmaller value previousIndex array =
+    array
+    |> Array.tryItem previousIndex
+    |> Option.map (fun previousValue -> previousValue < value)
+    |> Option.defaultValue false
 
 [<EntryPoint>]
 let main args =
-    printfn "Nice command-line arguments! Here's what System.Text.Json has to say about them:"
+    let filename =
+        Array.tryItem 0 args
+        |> Option.defaultValue "./input.txt"
 
+    let lines =
+        readLines filename
+        |> Array.map (fun line -> int line)
 
-    args |> Array.iter (fun item -> printfn "%s" item)
+    printfn "Read %i lines from %s" lines.Length filename
+
+    let largerThanPrevious =
+        lines
+        |> Array.indexed
+        |> Array.fold
+            (fun acc line ->
+                let (index, content) = line
+                let previousIndex = index - 1
+
+                if index = 0 then
+                    acc
+                elif isPreviousValueSmaller content previousIndex lines then
+                    acc + 1
+                else
+                    acc)
+            0
+
+    printfn "%i values are larger than the previous" largerThanPrevious
 
     0 // return an integer exit code
